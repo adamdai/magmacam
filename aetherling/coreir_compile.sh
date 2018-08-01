48d4483de299ddbaffc624921ef35f6a834567a9
David Durst <davidbdurst@gmail.com>
2018-08-01 11:33:09 -0700

    loading shared libs by name rather than path

--- aetherling/coreir_compile.sh
+++ aetherling/coreir_compile.sh
@@ -3,4 +3,4 @@
 #  2. board version / output directory (IceStick or HX8K)
 # edit output directory for each user
 
-coreir -i $1.json -o ../$2/build/$1.v -p 'rungenerators,wireclocks-coreir' -n 'aetherlinglib,commonlib,mantle,coreir,global' --load_libs '/usr/lib/libcoreir-aetherlinglib.so,/usr/lib/libcoreir-commonlib.so'
+coreir -i $1.json -o ../$2/build/$1.v -p 'rungenerators,wireclocks-coreir' -n 'aetherlinglib,commonlib,mantle,coreir,global' --load_libs 'aetherlinglib,commonlib'
